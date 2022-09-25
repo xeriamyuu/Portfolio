c1cd1f2ab1113ede6cb929ab02f923a0c56f390c
xeriamyuu <66210935+xeriamyuu@users.noreply.github.com>
2022-09-25 23:57:50 +1000

    Commit Portfolio

---
+++ components/ContactMe.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+type Props = {};
+
+function ContactMe({}: Props) {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto: kevin.chen19981@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} `;
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I have got just what you need.{" "}
+          <span className="decoration-[#F7AB0A]/50 underline">
+            Let&apos;s Talk.
+          </span>
+        </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+61466080967</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">kevin.chen19981@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">123 Developer Lane</p>
+          </div>
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className=" flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="Email"
+            />
+          </div>
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </motion.div>
+  );
+}
+
+export default ContactMe;
---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { urlFor } from "../sanity";
+import { Experience } from "../typings";
+
+type Props = {
+  experience: Experience;
+};
+
+function ExperienceCard({ experience }: Props) {
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] lg:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        src={urlFor(experience?.companyImage).url()}
+        alt=""
+      />
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">Technology Graduate</h4>
+        <p className="font-bold text-2xl mt-1">Accenture</p>
+        <div className="flex space-x-2 my-2">
+          {experience.technologies.map((technology) => (
+            <img
+              key={technology._id}
+              className="h-10 w-10 rounded-full"
+              src={urlFor(technology.image).url()}
+            />
+          ))}
+        </div>
+        <p className="uppercase py-5 text-gray-300">
+          {new Date(experience.dateStarted).toDateString()} -{" "}
+          {experience.isCurrentlyWorkingHere
+            ? "Present"
+            : new Date(experience.dateEnded).toDateString()}
+        </p>
+        <ul className="list-disc space-y-4 ml-5 text-lg h-80 w-4/5 overflow-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80 pr-5">
+          {experience.points.map((point, i) => (
+            <li key={i}>{point}</li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default ExperienceCard;
